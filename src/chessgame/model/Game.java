19cf3e3d9b4a671083b4703175c6bab80d3e9972
Trey-Mac <jmccloskey@ycp.edu>
2022-03-18 19:21:01 -0400

    gets and sets for game.java
    just gets and sets for game.java

--- src/chessgame/model/Game.java
+++ src/chessgame/model/Game.java
@@ -1,5 +1,48 @@
 package chessgame.model;
 
 public class Game{
-		
+	private ChessBoard chessBoard, previousStates[];
+	private Player player1, player2;
+	private boolean isSavedGame;
+	
+	public Game() {
+		chessBoard = new ChessBoard();
+		//previousStates = new ChessBoard();
+		isSavedGame = false;
+	}
+	
+	public void setChessBoard(ChessBoard chessBoard) {
+		this.chessBoard = chessBoard;
+	}
+	
+	public ChessBoard getChessBoard() {
+		return chessBoard;
+	}
+	
+	public void setPlayer1(Player player) {
+		player1 = player;
+	}
+	
+	public Player getPlayer1() {
+		return player1;
+	}
+	
+	public void setPlayer(Player player) {
+		this.player2 = player;
+	}
+	
+	public Player getPlayer2() {
+		return player2;
+	}
+	
+	public void setIsSavedGame(boolean bool) {
+		isSavedGame = bool;
+	}
+	
+	public boolean getIsSavedGame() {
+		return isSavedGame;
+	}
+	
+	
+	
 }
