153d4689bfdb6d0b56df03428859864f16bde9d9
Trey-Mac <jmccloskey@ycp.edu>
2022-03-25 19:37:05 -0400

    tile gets and sets and create board for chessboard
    tile class gets and sets.
    
    gives the board array of tiles a color and sets their x and y location in the method createBoard().

--- src/chessgame/model/ChessBoard.java
+++ src/chessgame/model/ChessBoard.java
@@ -6,4 +6,30 @@ public class ChessBoard {
 	public ChessBoard() {
 		board = new Tile[8][8];
 	}
+	
+	public void createBoard() {
+		for(int i = 0; i < 7; i++) {
+			for (int j = 0; j < 7; j++) {
+				if(i%2 == 0) {
+					if(j%2 == 0) {
+						board[i][j].setBlack();
+					}
+					else {
+						board[i][j].setWhite();
+					}
+				}
+				else {
+					if(j%2 == 1) {
+						board[i][j].setBlack();
+					}
+					else {
+						board[i][j].setWhite();
+					}
+				}
+				board[i][j].setXLocation(j);
+				board[i][j].setYLocation(i);
+			}
+		}
+	}
+	
 }
--- src/chessgame/model/Tile.java
+++ src/chessgame/model/Tile.java
@@ -1,5 +1,32 @@
 package chessgame.model;
 
 public class Tile {
-
+	private boolean color;//true = white//false = black
+	private int xLocation;
+	private int yLocation;
+	
+	public Tile() {
+	}
+	
+	public void setWhite() {
+		color = true;
+	}
+	public void setBlack() {
+		color = false;
+	}
+	public boolean getColor() {
+		return color;
+	}
+	public void setXLocation(int xLoc) {
+		xLocation = xLoc;
+	}
+	public int getXlocation() {
+		return xLocation;
+	}
+	public void setYLocation(int yLoc) {
+		yLocation = yLoc;
+	}
+	public int getylocation() {
+		return yLocation;
+	}
 }
