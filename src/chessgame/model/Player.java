cff18373ecf7496285d392d954ecff0ac5a5ef0b
Trey-Mac <jmccloskey@ycp.edu>
2022-03-26 14:29:48 -0400

    updated player
    updated player

--- src/chessgame/model/Player.java
+++ src/chessgame/model/Player.java
@@ -1,5 +1,26 @@
 package chessgame.model;
 
 public class Player {
-
+	private boolean color;
+	private ChessPiece[] piece;
+	
+	public Player() {
+		piece = new ChessPiece[16];
+	}
+	
+	public ChessPiece getPiece(int pieceNumber) {
+		return piece[pieceNumber];
+	}
+	
+	public void setPiece(int pieceNumber, ChessPiece piece) {
+		this.piece[pieceNumber] = piece;
+	}
+	
+	public boolean getColor() {
+		return color;
+	}
+	public void setColor(boolean color) {
+		this.color = color;
+	}
+	
 }
