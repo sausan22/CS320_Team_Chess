55edbdde9c9af681f1838354d99e90c9ad3dc990
Trey-Mac <jmccloskey@ycp.edu>
2022-03-27 20:30:04 -0400

    knight movement validation
    check move done for knight (just hardcoded the values in there)

--- src/chessgame/model/KnightPiece.java
+++ src/chessgame/model/KnightPiece.java
@@ -17,6 +17,25 @@ public class KnightPiece extends ChessPiece {
 	@Override
 	public boolean checkMove(int newx, int newy, ChessBoard cb) {
 		// TODO Auto-generated method stub
+		
+		//checking out of bounds
+		if(newx < 0 || newy < 0 || newx > 7 || newy > 7) {
+			return false;
+		}
+		
+		if((newx == this.getXlocation() + 2 && newy == this.getYlocation() + 1) || //up 2 left 1
+				((newx == this.getXlocation() + 2 && newy == this.getYlocation() - 1) ||  // up 2 right 1
+				(newx == this.getXlocation() - 2 && newy == this.getYlocation() + 1) ||  // down 2 left 1
+				(newx == this.getXlocation() - 2 && newy == this.getYlocation() - 1) ||  // down 2 right 1
+				(newx == this.getXlocation() + 1 && newy == this.getYlocation() + 2) ||  // left 2 up 1
+				(newx == this.getXlocation() - 1 && newy == this.getYlocation() + 2) ||  // left 2 down 1
+				(newx == this.getXlocation() + 1 && newy == this.getYlocation() - 2) ||  // right 2 up 1
+				(newx == this.getXlocation() - 1 && newy == this.getYlocation() - 2)) && // right 2 up 1
+				(cb.getTile(newx, newy).getPiece() == null || 
+				(cb.getTile(newx, newy).getPiece() != null || cb.getTile(newx, newy).getPiece().getColor() != this.getColor()))) {
+			return true;
+			
+		}
 		return false;
 	}
 
