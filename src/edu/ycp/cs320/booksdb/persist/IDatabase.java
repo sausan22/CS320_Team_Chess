69f5bdd3a71557fb24ef297df036aa92eea63116
dile0 <dile@ycp.edu>
2022-04-22 18:26:44 -0400

    Update IDatabase.java
    Added some search functions

--- src/edu/ycp/cs320/booksdb/persist/IDatabase.java
+++ src/edu/ycp/cs320/booksdb/persist/IDatabase.java
@@ -6,5 +6,9 @@ import edu.ycp.cs320.booksdb.model.*;
 import chessgame.model.ChessPiece;
 
 public interface IDatabase {
-	public List<Pair<UserDB, GameDB>> findAllUsersGames(int userID, int gameID);
+	public List<Pair<UserDB, GameDB>> findGameByUserID(int userID);
+	public List<Pair<PiecesDB, MovesDB>> findCoordinateByPieceNumber(int pieceNumber);
+	public List<Pair<PlayersDB, PiecesDB>> findPieceOwnerByColor(boolean color, int pieceNumber, int gameID);
+	public List<Pair<GameDB,MovesDB>> findGameSetUpByTurn(int gameID, int turn);
+	public List<Pair<PlayersDB, GameDB>> findPlayersByGameID(int gameID);
 }
