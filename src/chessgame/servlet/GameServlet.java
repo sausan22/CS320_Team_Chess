0feac30001398d7e4837c9a246959e22bf94c3f5
nonkygon <nchristman@ycp.edu>
2022-04-24 21:21:36 -0400

    Messed with game front end
    -can get intiial and final tiles in jsp, don't know how to bring to
    servlet yet

--- src/chessgame/servlet/GameServlet.java
+++ src/chessgame/servlet/GameServlet.java
@@ -2,6 +2,7 @@ package chessgame.servlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 //import javax.servlet.http.HttpServlet;
@@ -11,7 +12,6 @@ import javax.servlet.http.*;
 
 import chessgame.controller.*;
 import chessgame.model.*;
-import chessgame.model.ChessBoard;
 import edu.ycp.cs320.chessdb.PiecesByGameQuery;
 
 public class GameServlet extends HttpServlet {
@@ -37,6 +37,14 @@ public class GameServlet extends HttpServlet {
 		int xMove = -1;
 		int yMove = -1;
 		
+		try {
+			System.out.println("initial position is "+req.getParameter("ipos"));
+			System.out.println("final position is "+req.getParameter("fpos"));
+		}
+		catch (Exception NullPointerException) {
+			System.out.println("something has gone wrong");
+		}
+		
 		Game model = new Game();
 		//normally setGame would be called once
 		
@@ -54,7 +62,7 @@ public class GameServlet extends HttpServlet {
 		String[][] daBoard = new String[8][8];
 		//in the actual implementation, pieces would pull from the pieces array in game
 		//fake chessboard loading
-		/*ChessPiece[] pieces = {fakeChessPiece(0, 0, 24, true), fakeChessPiece(1, 0, 16, true), 
+		ChessPiece[] daPieces = {fakeChessPiece(0, 0, 24, true), fakeChessPiece(1, 0, 16, true), 
 				fakeChessPiece(2, 0, 20, true), fakeChessPiece(3, 0, 28, true), 
 				fakeChessPiece(4, 0, 30, true), fakeChessPiece(5, 0, 21, true), 
 				fakeChessPiece(6, 0, 17, true), fakeChessPiece(7, 0, 25, true),
@@ -69,9 +77,10 @@ public class GameServlet extends HttpServlet {
 				fakeChessPiece(0, 7, 26, false), fakeChessPiece(1, 7, 18, false), 
 				fakeChessPiece(2, 7, 22, false), fakeChessPiece(3, 7, 29, false), 
 				fakeChessPiece(4, 7, 31, false), fakeChessPiece(5, 7, 23, false), 
-				fakeChessPiece(6, 7, 19, false), fakeChessPiece(7, 7, 27, false)};*/
+				fakeChessPiece(6, 7, 19, false), fakeChessPiece(7, 7, 27, false)};
+		ArrayList<ChessPiece> pieces = new ArrayList<ChessPiece>(Arrays.asList(daPieces));
 		//REAL chessboard loading
-		ArrayList<ChessPiece> pieces = controller.getThePieces();
+		//ArrayList<ChessPiece> pieces = controller.getThePieces();
 		ChessBoard loadedBoard = model.getChessBoard();
 		for(int i = 0; i < 8; i++) {
 			for(int j = 0; j < 8; j++) {
@@ -103,55 +112,10 @@ public class GameServlet extends HttpServlet {
 			if(pieces.get(i).getColor()) {
 				daColor = "w";
 			}
+			
 			pieceId = "images/"+daColor+daClass+".png";
-			/*if(pNum>=0 && pNum<=15) {
-				if(!pieces.get(i).getColor()) {
-					rNum -= 8;
-				}
-				pieceId = ("images/"+daColor+"Pawn.png");
-				//pieceId = (daColor + "pawn" + rNum);
-			}
-			if(pNum>=16 && pNum<=19) {
-				rNum -= 16;
-				if(!pieces.get(i).getColor()) {
-					rNum -= 2;
-				}
-				pieceId = ("images/"+daColor+"Knight.png");
-				//pieceId = (daColor + "knight" + rNum);
-			}
-			if(pNum>=20 && pNum<=23) {
-				rNum -= 20;
-				if(!pieces.get(i).getColor()) {
-					rNum -= 2;
-				}
-				pieceId = ("images/"+daColor+"Bishop.png");
-				//pieceId = (daColor + "bishop" + rNum);
-			}
-			if(pNum>=24 && pNum<=27) {
-				rNum -= 24;
-				if(!pieces.get(i).getColor()) {
-					rNum -= 2;
-				}
-				pieceId = ("images/"+daColor+"Rook.png");
-				//pieceId = (daColor + "rook" + rNum);
-			}
-			if(pNum>=28 && pNum<=29) {
-				rNum -= 28;
-				if(!pieces.get(i).getColor()) {
-					rNum --;
-				}
-				pieceId = ("images/"+daColor+"Queen.png");
-				//pieceId = (daColor + "queen" + rNum);
-			}
-			if(pNum>=30 && pNum<=31) {
-				rNum -= 30;
-				if(!pieces.get(i).getColor()) {
-					rNum --;
-				}
-				pieceId = ("images/"+daColor+"King.png");
-				//pieceId = (daColor + "king" + rNum);
-			}*/
-			System.out.println(pieceId);
+			
+			//System.out.println(pieceId + " at position (" + pieces.get(i).getXlocation() + ", " + pieces.get(i).getYlocation() + ").");
 			daBoard[pieces.get(i).getXlocation()][pieces.get(i).getYlocation()] = pieceId;
 		}
 		
@@ -159,8 +123,8 @@ public class GameServlet extends HttpServlet {
 		for(int i = 0; i < daBoard.length; i++) {
 			for(int j = 0; j < daBoard[i].length; j++) {
 				String tileName = ("tile" + ((8*i)+j));
-				req.setAttribute(tileName, daBoard[i][j]);
-				System.out.println(tileName);
+				req.setAttribute(tileName, daBoard[j][i]);
+				System.out.println(tileName + " should be " + daBoard[i][j]);
 			}
 		}
 		//the fix pieces thing is just bc the values need to be loaded for some reason???
@@ -275,13 +239,27 @@ public class GameServlet extends HttpServlet {
 			return ret;
 		}
 	}
-	/*public ChessPiece fakeChessPiece(int xPos, int yPos, int pNum, boolean color) {
-		ChessPiece piece = new ChessPiece();
-		piece.setXlocation(xPos);
-		piece.setYlocation(yPos);
-		piece.setPieceNumber(pNum);
-		piece.setColor(color);
-		return piece;
-	}*/
+	public ChessPiece fakeChessPiece(int xPos, int yPos, int pNum, boolean color) {
+		ChessPiece daPiece = new PawnPiece(1, 1, true, 1);
+		if(pNum>=0 && pNum<=15) {
+			daPiece = new PawnPiece(xPos, yPos, color, pNum);
+		}
+		if(pNum>=16 && pNum<=19) {
+			daPiece = new KnightPiece(xPos, yPos, color, pNum);
+		}
+		if(pNum>=20 && pNum<=23) {
+			daPiece = new BishopPiece(xPos, yPos, color, pNum);
+		}
+		if(pNum>=24 && pNum<=27) {
+			daPiece = new RookPiece(xPos, yPos, color, pNum);
+		}
+		if(pNum>=28 && pNum<=29) {
+			daPiece = new QueenPiece(xPos, yPos, color, pNum);
+		}
+		if(pNum>=30 && pNum<=31) {
+			daPiece = new KingPiece(xPos, yPos, color, pNum);
+		}
+		return daPiece;
+	}
 }
 
