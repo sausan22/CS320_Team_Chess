5ff6906ee0f457c8c292b9d90a273be1b91dbb4f
Trey-Mac <jmccloskey@ycp.edu>
2022-05-09 19:23:56 -0400

    fixed knight piece
    uncommented tests and fixed knight validation.

--- junit/chessgame/model/BishopChessPieceTest.java
+++ junit/chessgame/model/BishopChessPieceTest.java
@@ -26,28 +26,27 @@ public class BishopChessPieceTest {
 	
 	@Test
 	public void testBishopCheckMove1() {
-		fail();
-//		assertTrue(BishopTest.checkMove(7, 7, chessBoard));
-//		assertTrue(BishopTest.checkMove(0,0, chessBoard));
-//		assertTrue(BishopTest.checkMove(1,7, chessBoard));
-//		assertTrue(BishopTest.checkMove(7,1, chessBoard));
-//		//outof bounds movement
-//		assertFalse(BishopTest.checkMove(8, 8, chessBoard));
-//		assertFalse(BishopTest.checkMove(-1, -1, chessBoard));
-//		//incorrect movement
-//		assertFalse(BishopTest.checkMove(5, 6, chessBoard));
-//		assertFalse(BishopTest.checkMove(3, 4, chessBoard));
-//		assertFalse(BishopTest.checkMove(5, 4, chessBoard));
-//		
-//		//piece in the way of friendly type
-//		temp.setxLocation(2);
-//		temp.setylocation(2);
-//		chessBoard.setTile(2, 2, temp);
-//		assertFalse(BishopTest.checkMove(2, 2, chessBoard));
-//		//of opposite type
-//		temp.setColor(false);
-//		chessBoard.setTile(2, 2, temp);
-//		assertTrue(BishopTest.checkMove(2, 2, chessBoard));
+		assertTrue(BishopTest.checkMove(7, 7, chessBoard));
+		assertTrue(BishopTest.checkMove(0,0, chessBoard));
+		assertTrue(BishopTest.checkMove(1,7, chessBoard));
+		assertTrue(BishopTest.checkMove(7,1, chessBoard));
+		//outof bounds movement
+		assertFalse(BishopTest.checkMove(8, 8, chessBoard));
+		assertFalse(BishopTest.checkMove(-1, -1, chessBoard));
+		//incorrect movement
+		assertFalse(BishopTest.checkMove(5, 6, chessBoard));
+		assertFalse(BishopTest.checkMove(3, 4, chessBoard));
+		assertFalse(BishopTest.checkMove(5, 4, chessBoard));
+		
+		//piece in the way of friendly type
+		temp.setxLocation(2);
+		temp.setylocation(2);
+		chessBoard.setTile(2, 2, temp);
+		assertFalse(BishopTest.checkMove(2, 2, chessBoard));
+		//of opposite type
+		temp.setColor(false);
+		chessBoard.setTile(2, 2, temp);
+		assertTrue(BishopTest.checkMove(2, 2, chessBoard));
 	}
 	 
 
